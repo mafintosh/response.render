917dfcb54aa1fa764b71f630f73fca67122e71ae
mafintosh <mathiasbuus@gmail.com>
2012-12-10 23:42:06 +0100

    defaulting to index template

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ module.exports = function(app) {
 	app.use('response.render', function(name, locals) {
 		var response = this;
 
+		if (typeof name === 'object') {
+			locals = name;
+			name = 'index';
+		}
+
 		pejs.render(name, locals || {}, function(err, result) {
 			if (err) return response.error(err);
 			response.send(result);
